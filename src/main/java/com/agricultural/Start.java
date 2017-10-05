6e5d3845fd30655fbf0ea32008fed2b65ca86c55
AlexeyShubenko <alexone350@gmail.com>
2017-10-05 22:35:33 +0300

    change operations window

--- src/main/java/com/agricultural/Start.java
+++ src/main/java/com/agricultural/Start.java
@@ -6,26 +6,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Alexey on 29.09.2017.
  */
 public class Start extends Application {
 
+
     public void start(Stage primaryStage) throws Exception {
 //        Parent parent = FXMLLoader.load(getClass().getResource("/views/start_page.fxml"));
-//        Parent parent = FXMLLoader.load(getClass().getResource("/views/operations.fxml"));
-        Parent parent = FXMLLoader.load(getClass().getResource("/views/machines.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/views/operations.fxml"));
+//        Parent parent = FXMLLoader.load(getClass().getResource("/views/machines.fxml"));
         Scene scene = new Scene(parent);
 
         primaryStage.setTitle("Agricultural");
-        primaryStage.setResizable(false);
+//        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
+        primaryStage.setMinHeight(480);
+        primaryStage.setMinWidth(450);
         primaryStage.sizeToScene();
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
+    public static void main(String[] args) {Application.launch(args);}
 
 }
